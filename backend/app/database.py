7e10cb0164eed9795f0f7bab44b9e05036684ae1
Diogo Barboza de Souza <diogobsouza2002@gmail.com>
2025-10-14 10:56:24 -0300

    arrumando bugs no backend dockerfile -> main.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #URL do banco de dados
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABSE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 #engine cominicação da sqlalchemy
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
